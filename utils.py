55afc97cb933abf8da3209336520dcb1a7ed2c31
Jinlin28 <144567569+Jinlin28@users.noreply.github.com>
2024-04-22 22:25:29 +0200

    add print head function

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
-data = pd.read_csv(data/titanic.csv)
+data = pd.read_csv('data/titanic.csv')
 
 def load_female_passengers():
     female_passengers = data[data['sex'] == 'female']
     return female_passengers
+print(load_female_passengers().head(10))
